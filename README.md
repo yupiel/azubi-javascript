c9e5cde443dac539ebe5ba050185831c5ab0db9b
Julian Godesa <julian.godesa@asideas.de>
2019-12-16 14:02:51 +0100

    adds control structure questions

--- README.md
+++ README.md
@@ -23,3 +23,13 @@ Now solve the test suite by fixing all the tests:
 
 ```npm run single -- datatypes```
 
+2. Control structures
+---------------------
+
+- what control structures do you know?
+- given a loop that prints number from 1 to 5. Implement this with a for loop and a while loop.
+- What is a fall-through in context of a switch statement?
+
+Now solve the test suite by fixing all the tests:
+
+```npm run single -- controlstructures```
---
+++ src/controlstructures/controlstructures.spec.js
@@ -0,0 +1,182 @@
+var assert = require('assert');
+
+describe('control structures', function () {
+    describe('if', function () {
+        it('single', function () {
+
+            let truthy = true;
+            let actual;
+
+            if (truthy) {
+                actual = 123;
+            }
+
+            let expected = '???';
+
+            assert.equal(actual, expected);
+        });
+
+        it('else', function () {
+
+            let truthy = true;
+            let actual;
+
+            if (truthy) {
+                actual = 123;
+            } else {
+                actual = 321;
+            }
+
+            let expected = '???';
+
+            assert.equal(actual, expected);
+        });
+
+        it('nested', function () {
+
+            let truthy = true;
+            let stillTruthy = true;
+            let actual;
+
+            if (truthy) {
+                actual = 123;
+                if (stillTruthy) {
+                    actual = 2222;
+                }
+            } else {
+                actual = 321;
+            }
+
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+    });
+
+    describe('switch', function () {
+        it('simple', function () {
+
+            let actual;
+            let given = 123;
+            switch(given) {
+                case '123': actual = 'first';
+                case "123": actual = 'second';
+                case 123: actual = 'third';
+            }
+
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+
+        it('simple with break', function () {
+
+            let actual;
+            let given = 123;
+            switch(given) {
+                case 123: {
+                    actual = 'first';
+                    break;
+                }
+                case 123: { 
+                    actual = 'second';
+                    break;
+                }
+            }
+
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+
+        it('simple default', function () {
+
+            let actual;
+            let given = 'no match';
+            switch(given) {
+                case 123: {
+                    actual = 'first';
+                    break;
+                }
+                case 123456: { 
+                    actual = 'second';
+                }
+                default: actual = 1;
+            }
+
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+
+    });
+
+    describe('for', function () {
+        it('single', function () {
+            let actual = 0;
+
+            for (var x = 0; x<10; x++) {
+                actual += x;
+            }
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+        
+        it('double', function () {
+            let actual = 0;
+
+            for (var x = 0, y = 0; x<10 && y<20; x++, y+=2) {
+                actual += x;
+            }
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+
+        it('for in', function () {
+            let actual = '';
+            let myObject = {
+                a: 12,
+                b: 23,
+            }
+            for (var key in myObject) {
+                 actual += key;
+            }
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+
+        it('for of', function () {
+            let actual = ''; // this is a string!
+            let myArray = [2,4,6,8];
+            for (var element of myArray) {
+                 actual += element;
+            }
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+
+        it('for of again', function () {
+            let actual = 0; // this is a number
+            let myArray = [2,4,6,8];
+            for (var element of myArray) {
+                 actual += element;
+            }
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+
+    });
+
+    describe('while', function () {
+        it('simple', function () {
+            
+            let actual = 0; 
+            let myArray = [2,4,6,8,1,2,3,4,5,6];
+            let i = 0;
+            
+            while (i < myArray.length) {
+                 actual *= myArray[i];
+                 i+=2;
+            }
+
+            let expected = '???';
+            assert.equal(actual, expected);
+        });
+    });
+});
